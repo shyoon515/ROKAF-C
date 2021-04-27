a366075c27863f65e01b2a14967b9f1e98fcb613
shyoon515 <james.sh.yoon@gmail.com>
2021-04-27 10:47:24 +0000

    today's 2nd commit

---
+++ src/2021_04/04_27/chatper04example02-FloatError.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    float num=0.0;
+    
+    for(i=0; i<100; i++)
+        num+=0.1;
+    
+    printf("0.1을 100번 더한 결과: %f \n", num);
+    return 0;
+}
