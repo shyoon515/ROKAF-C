e9a8f301a70e644f0f59011bb0eaacb5622b47fa
shyoon515 <james.sh.yoon@gmail.com>
2021-06-19 02:41:36 +0000

    chapter13 finished

---
+++ src/2021_06/06_19/chatper13example08-StringArray.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char * strArr[3]={"Simple", "String", "Array"};
+    printf("%s \n", strArr[0]);
+    printf("%s \n", strArr[1]);
+    printf("%s \n", strArr[2]);
+    return 0;
+}
